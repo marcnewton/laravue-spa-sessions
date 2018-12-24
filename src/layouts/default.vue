bb0a75c19d6898b19b8ebc91eaf19dbf5b9d6d41
Marc Newton <hello@marcnewton.co.uk>
2018-12-24 14:57:20 +0000

    Added vue default layout example.

---
+++ src/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div>
+        <h1>Vue Default Layout</h1>
+        <slot/>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+
+        name: 'layout-default'
+
+    }
+
+</script>
