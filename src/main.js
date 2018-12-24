98d7734e3db2bf5169fa4a9c3b794ee8ac89d928
Marc Newton <hello@marcnewton.co.uk>
2018-12-24 14:58:11 +0000

    added main.js

---
+++ src/main.js
@@ -0,0 +1,80 @@
+import Vue from 'vue'
+import { mapState } from 'vuex';
+
+/**
+ * https://lodash.com/docs
+ */
+
+import _ from 'lodash'
+Vue.prototype._ = _;
+
+/**
+ * Import extensions
+ */
+
+import router from './router';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+/**
+ * Import plugins
+ */
+
+import Http from './plugins/http';
+Vue.use(Http);
+
+import laravel from './plugins/laravel'
+Vue.use(laravel);
+
+
+/**
+ * Initialize the main application
+ */
+
+import App from './App';
+
+window.App = new Vue({
+
+    render: h => h(App),
+    store,
+    router,
+    laravel,
+
+    data() {
+
+        return {
+
+            loading: 0
+
+        }
+
+    },
+
+    beforeCreate() {
+
+        this.$Laravel.initialize();
+
+    },
+
+    computed: {
+
+        ...mapState(['user','isAuthenticated','isInitializing']),
+
+        isLoading () {
+
+            return this.loading > 0;
+
+        },
+
+        layout () {
+
+            return this.$route.meta.layout || 'layout-default';
+
+        }
+
+    }
+
+}).$mount('#app');
+
+import './scss/app.scss'
